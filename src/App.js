5c4f5c3449ea36e4bd244ec7e5277f726e0a8147
daisyzarzo <36684960+daisyzarzo@users.noreply.github.com>
2020-09-02 09:17:51 -0500

    primer comit del proyecto react.jq:Q 	|

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './styles/styles.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//Reglas JSX
+// 1 : todas las etiquetas deben cerrarse
+// 2: los componentes deben devolver un solo elemento paddre
+// 3: apoyarse de los fragmentso cuando necesito devolver 2 eelementos
+// 4: Fragment  =>  <> hijos </>
+
+
+
+const App = () => (
+  <div className="main-banner img-container" id="main-banner">
+    <div className="ed-grid lg-grid-6">
+      <div className="lg-cols-4 lg-x-2">
+        <img className="main-banner__img" src="" alt="imagen" />
+        <div className="main-banner__data s-center">
+          <p className="t2 s-mb-0">Titulo del banner</p>
+          <p>Subtitulo del banner</p>
+          <a href="#" className="button">Boton del banner</a>
+        </div>
+      </div>
     </div>
-  );
-}
-
+  </div>
+);
 export default App;
